e3af1aea6a285feafa27dd98d1c5c5d0476fb38c
Inzamam Shaikh <92295764+Inzamamdev@users.noreply.github.com>
2025-02-09 14:32:38 +0000

    host change

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],server: {
-    allowedHosts: ['x4ps5x-5173.csb.app'] // Allow all external hosts
-  }
+  plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [".csb.app"], // Allow all external hosts
+  },
 });
